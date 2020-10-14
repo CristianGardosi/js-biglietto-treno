a51a33eb855504c54920dc5b7417fa290d4c68e9
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-10-14 16:35:48 +0200

    INSERIMENTO PREZZI

--- js/script.js
+++ js/script.js
@@ -20,16 +20,19 @@ if ( isNaN(età) ){
 }
 
 // Prezzo base senza sconti
-var prezzoBase = ( km * 0.21 );
+var prezzo = ( km * 0.21 );
     console.log('Prezzo base biglietto: ', km * 0.21 + ' $');
 // Sconto over 65
 if (età > 65){
-    console.log('Prezzo biglietto over 65: ', prezzoBase * 0.60 + ' $');
+    console.log('Prezzo biglietto over 65: ', prezzo * 0.60 + ' $');
 }
 // Sconto under 18
 else if (età < 18 ){
-    console.log('Prezzo biglietto under 18: ', prezzoBase * 0.80 + ' $');
+    console.log('Prezzo biglietto under 18: ', prezzo * 0.80 + ' $');
 }
 
+
+
+document.getElementById('ticketprice').innerHTML = 'Il prezzo del biglietto per la tratta selezionata è di: ' + prezzo + '$';
 
 
